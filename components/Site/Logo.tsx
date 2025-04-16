2259b2675335ad78ad4c7d1448a894a54c927988
miracleonyenma <miracleficient@gmail.com>
2025-04-16 11:52:44 +0100

    style(Logo): update logo design and color scheme
    Refactor the SiteLogo component to use the StarterIcon and change the text color to blue. Update the StarterIcon SVG to use a new design with different dimensions and colors.

--- components/Site/Logo.tsx
+++ components/Site/Logo.tsx
@@ -1,10 +1,10 @@
+import StarterIcon from "@/components/Icon/Starter";
+
 const SiteLogo = () => {
   return (
-    <figure className="flex items-center gap-1 text-indigo-400 dark:text-indigo-400">
-      {/* <StarterIcon className="icon text-payred-06 h-12 w-12" /> */}
-      <figcaption className="mt-1 text-2xl font-extrabold">
-        Commitments
-      </figcaption>
+    <figure className="flex items-center gap-1 text-blue-600">
+      <StarterIcon className="icon text-payred-06 h-12 w-12" />
+      <figcaption className="text-2xl font-extrabold">Starter</figcaption>
     </figure>
   );
 };
