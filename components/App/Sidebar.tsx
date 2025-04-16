859dddaec21e612f31691b438ec5fd90d2da5a23
miracleonyenma <miracleficient@gmail.com>
2025-04-16 23:42:56 +0100

    feat(Sidebar): add secondary navigation and user button enhancements
    - Add secondary navigation items for "Support" and "Feedback"
    - Enhance AuthUserButton to support expanded and small states
    - Integrate user information display in the sidebar footer

--- components/App/Sidebar.tsx
+++ components/App/Sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Sidebar,
   SidebarContent,
@@ -11,13 +13,19 @@ import {
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import {
+  Comment01Icon,
   Home01Icon,
   InboxIcon,
+  LifebuoyIcon,
   Settings01Icon,
 } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
+import { JSX } from "react";
+import AuthUserButton from "../Auth/User/Button";
+import { useUserStore } from "@/store/useUserStore";
 
 export function AppSidebar() {
+  const { user } = useUserStore();
   // Menu items.
   const items = [
     {
@@ -44,13 +52,38 @@ export function AppSidebar() {
         />
       ),
     },
-
     {
       title: "Settings",
       url: "#",
       icon: (
         <HugeiconsIcon
           icon={Settings01Icon}
+          className="icon"
+          color="currentColor"
+          strokeWidth={2}
+        />
+      ),
+    },
+  ];
+  const secondaryItems = [
+    {
+      title: "Support",
+      url: "#",
+      icon: (
+        <HugeiconsIcon
+          icon={LifebuoyIcon}
+          className="icon"
+          color="currentColor"
+          strokeWidth={2}
+        />
+      ),
+    },
+    {
+      title: "Feedback",
+      url: "#",
+      icon: (
+        <HugeiconsIcon
+          icon={Comment01Icon}
           className="icon"
           color="currentColor"
           strokeWidth={2}
@@ -79,8 +112,41 @@ export function AppSidebar() {
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
+        <NavSecondary items={secondaryItems} className="mt-auto" />
       </SidebarContent>
-      <SidebarFooter />
+      <SidebarFooter>
+        <AuthUserButton user={user} expanded small />
+      </SidebarFooter>
     </Sidebar>
+  );
+}
+
+export function NavSecondary({
+  items,
+  ...props
+}: {
+  items: {
+    title: string;
+    url: string;
+    icon: JSX.Element;
+  }[];
+} & React.ComponentPropsWithoutRef<typeof SidebarGroup>) {
+  return (
+    <SidebarGroup {...props}>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton asChild size="sm">
+                <a href={item.url}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
   );
 }
--- components/Auth/User/Button.tsx
+++ components/Auth/User/Button.tsx
@@ -18,7 +18,31 @@ import useShortcut from "@/hooks/useShortcut";
 import { AuthUserLogoutDialog } from "@/components/Auth/User/LogoutDialog";
 import { useRouter } from "next/navigation";
 
-const AuthUserButton: React.FC<{ user?: User | null }> = ({ user }) => {
+const UserInfo: React.FC<{ user?: User | null; small?: boolean }> = ({
+  user,
+  small = false,
+}) => {
+  return (
+    <div className="flex flex-col p-2">
+      <span
+        className={`text text-left font-semibold ${small ? "text-sm" : ""}`}
+      >
+        {user?.firstName} {user?.lastName}
+      </span>
+      <span
+        className={`text-sm truncate text-gray-400 ${small ? "text-xs" : ""}`}
+      >
+        {user?.email}
+      </span>
+    </div>
+  );
+};
+
+const AuthUserButton: React.FC<{
+  user?: User | null;
+  expanded?: boolean;
+  small?: boolean;
+}> = ({ user, expanded = false, small = false }) => {
   const router = useRouter();
   const [logoutDialogOpen, setLogoutDialogOpen] = useState(false);
 
@@ -29,63 +53,60 @@ const AuthUserButton: React.FC<{ user?: User | null }> = ({ user }) => {
 
   return (
     <>
-      {user?.id ? (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <button className="btn ghost !p-0">
-              <Avatar className="h-10 w-10 rounded-full">
-                <AvatarImage
-                  src={user?.picture || "https://github.com/shadcn.png"}
-                  alt={user?.firstName || "user"}
-                />
-                <AvatarFallback>
-                  <span className="uppercase">
-                    {user.firstName?.charAt(0)}
-                    {user.lastName?.charAt(0)}
-                  </span>
-                </AvatarFallback>
-              </Avatar>
-            </button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end" className="w-72">
-            <DropdownMenuLabel>
-              <div className="flex flex-col p-2">
-                <span className="text-xl font-semibold">
-                  {user.firstName} {user.lastName}
-                </span>
-                <span className="text-sm text-gray-400">{user.email}</span>
-              </div>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem className="px-4 py-3" asChild>
-                <Link href={"/account"}>Account</Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem className="px-4 py-3" asChild>
-                <Link href={"/notes"}>Notes</Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem className="px-4 py-3" disabled>
-                Upgrade to Pro
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
+      {
+        user?.id ? (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <button className="btn ghost flex items-center !p-0 gap-2">
+                <Avatar className="h-10 w-10 rounded-full">
+                  <AvatarImage
+                    src={user?.picture || "https://github.com/shadcn.png"}
+                    alt={user?.firstName || "user"}
+                  />
+                  <AvatarFallback>
+                    <span className="uppercase">
+                      {user.firstName?.charAt(0)}
+                      {user.lastName?.charAt(0)}
+                    </span>
+                  </AvatarFallback>
+                </Avatar>
+                {expanded && <UserInfo user={user} small={small} />}
+              </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-72">
+              <DropdownMenuLabel>
+                <UserInfo user={user} />
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem className="px-4 py-3" asChild>
+                  <Link href={"/account"}>Account</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="px-4 py-3" asChild>
+                  <Link href={"/notes"}>Notes</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="px-4 py-3" disabled>
+                  Upgrade to Pro
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
 
-            <DropdownMenuSeparator />
-            <DropdownMenuItem
-              className="px-4 py-3"
-              onClick={() => {
-                setLogoutDialogOpen(true);
-              }}
-            >
-              Log out
-              <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      ) : (
-        <Link href="/auth/register" className="btn md primary">
-          Sign Up
-        </Link>
-      )}
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                className="px-4 py-3"
+                onClick={() => {
+                  setLogoutDialogOpen(true);
+                }}
+              >
+                Log out
+                <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        ) : null
+        // <Link href="/auth/register" className="btn md primary">
+        //   Sign Up
+        // </Link>
+      }
 
       <AuthUserLogoutDialog
         open={logoutDialogOpen}
